9c89ff91054825b9d1c0a9da845d109c1ac01536
gaozhidong <1011284295@qq.com>
2017-07-26 07:55:36 +0800

    add_task

--- js/base.js
+++ js/base.js
@@ -2,11 +2,38 @@
 (function () {
     'use strict';
 
-    var $form_add_task = $('.add-task')
+    var $form_add_task = $('.add-task'),
+        new_task = {},
+        task_list = {}
 
-    $form_add_task.on('submit', (e) => {
+        init()
+     $form_add_task.on('sbumit', function (e) {
+        /* 禁用默认行为 */
         e.preventDefault();
-        $(this).find('input[name=content]').val();
+        /* 获取新Task的值 */
+        new_task.content = $(this).find('input[name=content]').val();
+        /* 如果新Task的值为空 则直接返回 否则继续执行 */
+        if (!new_task.content) return;
+        console.log('new_task')
+        /* 存入新Task */
+        add_task(new_task)
     })
 
+    function add_task(new_task) {
+        /* 将新Task推入到task_list */
+        task_list.push(new_task);
+        /* 更新localStorate */
+        store.set('task_list', task_list)
+
+        console.log('task_list', task_list)
+    }
+
+    function init() {
+        task_list = store.get('task_list') || [];
+      //  console.log(22)
+    }
+
+    function s(){
+
+    }
 })()
