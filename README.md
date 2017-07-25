5a01951bc78ca2f3087c0c20e580deefc6082e5d
gaozhidong <1011284295@qq.com>
2017-07-26 07:14:45 +0800

    store.js

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 ## 使用localstorage 存储数据
 
-使用store.js插件
+使用store.js插件  https://github.com/marcuswestin/store.js/
--- js/base.js
+++ js/base.js
@@ -1,5 +1,12 @@
-;(function(){
+;
+(function () {
     'use strict';
-    console.log($)
+
+    var $form_add_task = $('.add-task')
+
+    $form_add_task.on('submit', (e) => {
+        e.preventDefault();
+        $(this).find('input[name=content]').val();
+    })
 
 })()
