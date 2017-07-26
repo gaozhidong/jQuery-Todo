15239cdeaa417240342edf07212f87d23ef58ffb
gaozhidong <1011284295@qq.com>
2017-07-26 23:19:16 +0800

    add task

--- js/base.js
+++ js/base.js
@@ -1,39 +1,60 @@
 ;
 (function () {
     'use strict';
+    var log = console.log.bind(console);
+    var $form_add_task = $('.add-task')
+    var new_task = {}
+    var task_list = {}
 
-    var $form_add_task = $('.add-task'),
-        new_task = {},
-        task_list = {}
 
-        init()
-     $form_add_task.on('sbumit', function (e) {
+
+
+    init()
+    $form_add_task.on('submit', function (e) {
         /* 禁用默认行为 */
         e.preventDefault();
         /* 获取新Task的值 */
         new_task.content = $(this).find('input[name=content]').val();
         /* 如果新Task的值为空 则直接返回 否则继续执行 */
         if (!new_task.content) return;
-        console.log('new_task')
         /* 存入新Task */
-        add_task(new_task)
+        if (add_task(new_task)) {
+            render_task_list()
+        }
+
     })
 
     function add_task(new_task) {
         /* 将新Task推入到task_list */
-        task_list.push(new_task);
+        task_list.push(new_task)
         /* 更新localStorate */
         store.set('task_list', task_list)
-
-        console.log('task_list', task_list)
+        return true;
     }
 
     function init() {
         task_list = store.get('task_list') || [];
-      //  console.log(22)
     }
 
-    function s(){
+    function render_task_list() {
+        var $task_list = $('.task-list');
 
+        for (var i = 0; i < task_list.length; i++) {
+            var $task = render_task_tpl(task_list[i])
+            $task_list.append($task)
+        }
+
+    }
+
+    function render_task_tpl(data) {
+        var list_item_tpl = `
+                <div class="task-item">		
+                    <span><input type="checkbox"></span>
+                    <span class="task-content">'{{data.content}}'</span>
+                    <span>删除</span>
+                    <span>详细</span>
+                </div>
+            `
+        return $(list_item_tpl)
     }
 })()
