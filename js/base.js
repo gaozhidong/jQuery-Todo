2752044b8b395ff4d4920799cf2012a2afed1d25
gaozhidong <1011284295@qq.com>
2017-07-28 06:52:29 +0800

    详情按钮

--- js/base.js
+++ js/base.js
@@ -4,11 +4,10 @@
     var log = console.log.bind(console);
     var $form_add_task = $('.add-task'),
         task_list = [],
-        $task_delete,
-        $task_detail,
-        $task_detail = $('task-detail'),
+        $task_delete_trigger,
+        $task_detail_trigger,
+        $task_detail = $('.task-detail'),
         $task_detail_mask = $('.task-detail-mask')
-
 
 
     init()
@@ -32,19 +31,24 @@
         }
     }
 
-    function listion_task_detail() {
-        $task_detail.on('click', function () {
+    function listen_task_detail() {
+        $task_detail_trigger.on('click', function () {
             var $this = $(this)
             var $item = $this.parent().parent()
             var index = $item.data('index')
-            log("index", index)
+            show_task_detail(index)
         })
+    }
+
+    function show_task_detail(index) {
+        $task_detail.show();
+        $task_detail_mask.show();
     }
 
 
     /* 查找并监听所有删除按钮的点击事件 */
     function listen_task_delete() {
-        $task_delete.on('click', function () {
+        $task_delete_trigger.on('click', function () {
             var $this = $(this);
             /* 找到删除按钮所在的task元素 */
             var item = $this.parent().parent();
@@ -97,10 +101,10 @@
             $task_list.append($task)
         }
 
-        $task_delete = $('.action.delete')
-        $task_detail = $('.action.detail')
+        $task_delete_trigger = $('.action.delete')
+        $task_detail_trigger = $('.action.detail')
         listen_task_delete();
-        listion_task_detail()
+        listen_task_detail()
     }
     /* 渲染单条Task模板 */
     function render_task_item(data, index) {
