19f9569f360c97c71f97ae25c936e462e7f74de9
gaozhidong <1011284295@qq.com>
2017-07-27 13:33:02 +0800

    添加注释

--- js/base.js
+++ js/base.js
@@ -2,19 +2,22 @@
 (function () {
     'use strict';
     var log = console.log.bind(console);
-    var $form_add_task = $('.add-task')
-    var task_list = {}
-    var $delete_task
+    var $form_add_task = $('.add-task'),
+        task_list = {},
+        $delete_task
 
 
 
     init()
-    $form_add_task.on('submit', function (e) {
-        var new_task = {}
+    $form_add_task.on('submit', on_add_task_from_submit)
+
+    function on_add_task_from_submit(e) {
+        var new_task = {},
+            $input;
         /* 禁用默认行为 */
         e.preventDefault();
         /* 获取新Task的值 */
-        var $input = $(this).find('input[name=content]')
+        $input = $(this).find('input[name=content]')
         new_task.content = $input.val();
 
         /* 如果新Task的值为空 则直接返回 否则继续执行 */
@@ -24,17 +27,16 @@
             //render_task_list();
             $input.val(null)
         }
-
-    })
-
+    }
+    /* 查找并监听所有删除按钮的点击事件 */
     function listen_task_delete() {
         $delete_task.on('click', function () {
             var $this = $(this);
+            /* 找到删除按钮所在的task元素 */
             var item = $this.parent().parent();
             var index = item.data('index');
-
+            /* 确认删除 */
             var tmp = confirm('确定删除？')
-
             tmp ? delete_task(index) : null;
 
         })
@@ -53,7 +55,7 @@
         store.set('task_list', task_list);
         render_task_list()
     }
-    //删除task
+    /* 删除一条Task */
     function delete_task(index) {
         /* 如果没有index 或者 index 不存在直接返回 */
         if (index === undefined || !task_list[index]) return;
@@ -69,10 +71,10 @@
         task_list = store.get('task_list') || [];
         if (task_list.length) {
             render_task_list();
-            
+
         }
     }
-
+    /* 渲染所有Task模板 */
     function render_task_list() {
         var $task_list = $('.tasks-list');
         $task_list.html('')
@@ -84,7 +86,7 @@
         $delete_task = $('.action.delete')
         listen_task_delete();
     }
-
+    /* 渲染单条Task模板 */
     function render_task_item(data, index) {
         if (!data || !index) return;
         var list_item_tpl = `
